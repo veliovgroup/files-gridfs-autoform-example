23876b036f198c285304d85ad20205ae5574aa36
Jan Küster <jkuester@uni-bremen.de>
2019-06-30 11:16:56 +0200

    integrated GridFSBucket into Images instead of deprecated gridfs-stream

--- server/main.js
+++ server/main.js
@@ -1,8 +1,1 @@
-import { Meteor }         from 'meteor/meteor';
-import { MongoInternals } from 'meteor/mongo';
-
-import grid from 'gridfs-stream';
-
-const Gfs = grid(Meteor.users.rawDatabase(), MongoInternals.NpmModules.mongodb.module);
-
-export { Gfs };
+import './publications';
